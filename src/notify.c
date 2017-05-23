2e4e160427643cc587e30259e255be0fcba6a9bf
Emil Nilsson <karl.emil.nilsson@gmail.com>
2017-05-23 17:19:00 +0200

    Add first draft of notify-send function

---
+++ src/notify.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+void send_notification(const char* summary, const char* body) 
+{
+	// app constants for notify-send
+	// TODO: move these to header file
+	const char* command = "notify-send";
+	const char* app = "Teamspeak 3";
+	
+	int extra = 15; // for use of spaces/formattign
+	char notify[strlen(command) + strlen(summary) + strlen(app) + 
+		strlen(body) + extra]
+
+	snprintf(notify, sizeof notify, "%s \"%s\" -a \"%s\" \"%s\"", command,
+			summary, app, body);
+	system(notify);
+}
---
+++ src/notify.h
@@ -0,0 +1,9 @@
+#ifndef NOTIFY_H
+#define NOTIFY_H
+
+/**
+ * Sends a notification to user using libnotify's notify-send.
+ * Takes the arguments summary and body that will be passed to
+ * notify-send as summary and body respectively.
+ */
+void send_notification(const char* summary, const char* body);
