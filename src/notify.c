c7605fc3aa2e51fe6cf3f9863f88e644ebdd00a8
Emil Nilsson <karl.emil.nilsson@gmail.com>
2017-11-11 23:17:47 +0100

    Add missing depenencies and fix syntax in notify

--- src/notify.c
+++ src/notify.c
@@ -1,5 +1,7 @@
 #include "notify.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 static const char* COMMAND_NOTIFY = "notify-send";
 static const char* APP_NAME = "TeaamSpeak 3";
@@ -7,8 +9,8 @@ static const char* APP_NAME = "TeaamSpeak 3";
 void notify(const char *summary, const char *body, int urgency_level)
 {
 	int extra = 15; // for use of spaces/formattign
-	char notify[strlen(command) + strlen(summary) + strlen(app) + 
-		strlen(body) + extra]
+	char notify[strlen(COMMAND_NOTIFY) + strlen(summary) + strlen(APP_NAME) + 
+		strlen(body) + extra];
 
 	snprintf(notify, sizeof notify, "%s \"%s\" -a \"%s\" \"%s\"",
 			COMMAND_NOTIFY, summary, APP_NAME, body);
@@ -18,13 +20,13 @@ void notify(const char *summary, const char *body, int urgency_level)
 void notify_server_message(const char *server, const char *sender,
 		const char *message)
 {
-	char *server_suffix = " MESSAGE";
-	char *summary[strlen(server) + strlen(server_suffix) + 1];
+	const char *server_suffix = " MESSAGE";
+	char summary[strlen(server) + strlen(server_suffix) + 1];
 	strcpy(summary, server);
 	strcat(summary, server_suffix);
 
-	char *separator = ": ";
-	char *body[strlen(sender) + strlen(separator) + strlen(message) + 1];
+	const char *separator = ": ";
+	char body[strlen(sender) + strlen(separator) + strlen(message) + 1];
 	strcpy(body, sender);
 	strcat(body, separator);
 	strcat(body, message);
@@ -35,13 +37,13 @@ void notify_server_message(const char *server, const char *sender,
 void notify_channel_message(const char *channel, const char *sender,
 		const char *message)
 {
-	char *channel_suffix = " MESSAGE";
-	char *summary[strlen(channel) + strlen(channel_suffix) + 1];
+	const char *channel_suffix = " MESSAGE";
+	char summary[strlen(channel) + strlen(channel_suffix) + 1];
 	strcpy(summary, channel);
 	strcat(summary, channel_suffix);
 
-	char *separator = ": ";
-	char *body[strlen(sender) + strlen(separator) + strlen(message) + 1];
+	const char *separator = ": ";
+	char body[strlen(sender) + strlen(separator) + strlen(message) + 1];
 	strcpy(body, sender);
 	strcat(body, separator);
 	strcat(body, message);
@@ -51,11 +53,11 @@ void notify_channel_message(const char *channel, const char *sender,
 
 void notify_private_message(const char *sender, const char *message)
 {
-	char *summary = "PRIVATE MESSAGE";
-	char *separator = ": ";
+	const char *summary = "PRIVATE MESSAGE";
+	const char *separator = ": ";
 	char body[strlen(sender) + strlen(separator) + strlen(message) + 1];
 	strcpy(body, sender);
-	strncat(body, separator);
+	strcat(body, separator);
 	strcat(body, message);
 
 	notify(summary, body, 1);
