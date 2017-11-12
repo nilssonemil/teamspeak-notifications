49226d3eb2de7d2755724b0719f62a5fdf9e9294
Emil Nilsson <karl.emil.nilsson@gmail.com>
2017-11-12 20:59:04 +0100

    Remove comments from notify.h

--- src/notify.h
+++ src/notify.h
@@ -6,28 +6,15 @@
  */
 typedef enum {LOW, NORMAL, CRITICAL} urgency_t;
 
-/**
- * Sends a notification to user using libnotify's notify-send.
- * Takes the arguments summary and body that will be passed to
- * notify-send as summary and body respectively.
- */
-void notify(const char *summary, const char *body,
-    urgency_t urgency);
-
+void notify(const char *summary, const char *body, urgency_t urgency);
 void notify_server_message(const char *server, const char *sender,
     const char *message);
-
 void notify_channel_message(const char *channel, const char *sender,
     const char *message);
-
 void notify_private_message(const char *sender, const char *message);
-
 void notify_join(const char *client);
-
 void notify_leave(const char *client);
-
 void notify_poke(const char *poker, const char *message);
-
 void notify_channel_edited(const char *editer);
 
 #endif
